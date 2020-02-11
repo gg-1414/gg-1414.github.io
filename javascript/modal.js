5d0808cf0c5e5b62aad12789be859dc76ebf49b5
Gina Lee <gina@thesistesting.com>
2020-02-10 19:54:12 -0500

    working audio visualizer backgroundgit add . desktop only (#5)

--- javascript/modal.js
+++ javascript/modal.js
@@ -1,16 +1,3 @@
-function playAudio() {
-  // have player appear fixed to bottom of screen 
-  audioEl.classList += 'active'
-
-  // audio setup  
-  // audio.src = audioFiles[0]
-  // audio.load()
-  // audio.play()
-
-  audioSetup(playlistIndex) 
-  // audio play 
-}
-
 function closeModal() {
   modalContainer.classList = ''
   modalContainer.classList += 'out'
